9f4ae06ae8edc4d8fe9f67e899d7bf28eca9e104
Sylvain Leclercq <maisbiensurqueoui@gmail.com>
2023-06-17 20:25:50 +0200

    Initial commit

---
+++ app/src/main/java/dpsg/conduit/MainActivity.kt
@@ -0,0 +1,11 @@
+package dpsg.conduit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
