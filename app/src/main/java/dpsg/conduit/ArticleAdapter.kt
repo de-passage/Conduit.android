4af323f2499ead6ca5549283c4a6e3191e30bdde
Sylvain Leclercq <maisbiensurqueoui@gmail.com>
2023-06-22 08:11:23 +0200

    Style the main page

--- app/src/main/java/dpsg/conduit/ArticleAdapter.kt
+++ app/src/main/java/dpsg/conduit/ArticleAdapter.kt
@@ -3,14 +3,19 @@ package dpsg.conduit
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import dpsg.conduit.api.models.Article
+import org.w3c.dom.Text
 
 class ArticleAdapter(private var articles: List<Article>) : RecyclerView.Adapter<ArticleAdapter.ArticleViewHolder>() {
     class ArticleViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val title: TextView = itemView.findViewById(R.id.title_text_view)
         val summary: TextView = itemView.findViewById(R.id.article_excerpt_text_view)
+        val authorAvatar: ImageView = itemView.findViewById(R.id.article_author_pic)
+        val author: TextView = itemView.findViewById(R.id.article_author_text_view)
     }
 
     fun setArticles(articles: List<Article>) {
@@ -32,5 +37,9 @@ class ArticleAdapter(private var articles: List<Article>) : RecyclerView.Adapter
         val article = articles[position]
         holder.title.text = article.title
         holder.summary.text = article.description
+        Glide.with(holder.itemView.context)
+            .load(article.author.image)
+            .into(holder.authorAvatar)
+        holder.author.text = article.author.username
     }
 }
---
+++ app/src/main/java/dpsg/conduit/ShowArticle.kt
@@ -0,0 +1,11 @@
+package dpsg.conduit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ShowArticle : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_show_article)
+    }
+}
